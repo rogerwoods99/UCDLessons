6e3ed692149a7901c163c1f3698fc97ffac3f956
rwoods <rogerwoods99@hotmail.com>
2021-02-04 17:00:32 +0000

    commit 04022021

--- UCDLessons/DC_Hacker.py
+++ UCDLessons/DC_Hacker.py
@@ -84,3 +84,54 @@ plt.plot(random_walk)
 
 # Show the plot
 plt.show()
+plt.clf()
+
+#=================================================
+# Now show 10 walks on one graph
+
+# initialize and populate all_walks
+all_walks = []
+for i in range(100) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+            # Implement clumsiness
+        if np.random.rand() <= 0.001:  # add some randomness that go to zero 0.1% of time
+            step = 0
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Convert all_walks to Numpy array: np_aw
+np_aw=np.array(all_walks)
+
+# Plot np_aw and show
+#plt.plot(np_aw)
+#plt.show()
+
+# Clear the figure
+#plt.clf()
+
+# Transpose np_aw: np_aw_t
+
+np_aw_t=np.transpose(np_aw)
+# Plot np_aw_t and show
+
+plt.plot(np_aw_t)
+#plt.xscale('log')
+plt.xlabel('Run no.')
+plt.ylabel('Step position')
+plt.title('Various trajectories')
+#plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
+#plt.text(1550, 71, 'India')   # add text on graph at particular points
+#plt.text(5700, 80, 'China')
+#plt.grid(True)   # show grid lines
+plt.show()
+
+print(np_aw_t[-1].size)  # the size of the final row in the array
